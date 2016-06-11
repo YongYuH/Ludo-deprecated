e9ef62487e8625e25aa2d183c8261975573076fd
YongYuH <larry830326@hotmail.com>
2016-06-12 00:11:43 +0800

    modify responsive card design

--- js/Ludo_Playground.js
+++ js/Ludo_Playground.js
@@ -1,27 +1,14 @@
 function card_based_content_responsive() {
     var screen_width = $(window).width()
+    var content_padding_x = 20;
+    var sidebar_width = 100;
+    var card_width = 230;
+    var card_padding_x = 15;
     var total_column = 0;
-    switch (true) {
-        case screen_width >= 1285:
-            total_column = 6;
-            break;
-        case screen_width >= 1095:
-            total_column = 5;
-            break;
-        case screen_width >= 930:
-            total_column = 4;
-            break;
-        case screen_width >= 770:
-            total_column = 3;
-            break;
-        case screen_width >= 550:
-            total_column = 2;
-            break;
-        case screen_width < 550:
-            total_column = 1;
 
-            break;
-    }
+    var rwd_variable = (screen_width - content_padding_x + card_padding_x - sidebar_width)/(card_width + card_padding_x);
+    total_column = Math.floor(rwd_variable);
+
     var total_height_of_column = new Array(total_column); 
 
     // array initiation
@@ -32,7 +19,7 @@ function card_based_content_responsive() {
     var post = 0;
     var post_height = 0;
     var current_col = 0;
-    var current_left_distance = 0;
+    var current_card_distance_x = 0;
 
     var main = $('.main-content');
     var total_post = main.children().length;
@@ -42,8 +29,8 @@ function card_based_content_responsive() {
         current_col = post_num % total_column;
 
         // x axis
-        current_left_distance = 100 / total_column * current_col + '%';
-        $('.column-' + post_num).css('left', current_left_distance);
+        current_card_distance_x = current_col * (card_width + card_padding_x);
+        $('.column-' + post_num).css('left', current_card_distance_x);
 
         // y axis
         $('.post-'+post_num).css('top', total_height_of_column[current_col]);
