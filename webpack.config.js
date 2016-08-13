fc4f7b977514d4cb857be123464309dda8aff736
YongYuH <larry830326@hotmail.com>
2016-08-13 20:56:51 +0800

    [Modify] webpack config coding style

--- webpack.config.js
+++ webpack.config.js
@@ -74,44 +74,44 @@ var config;
 
 // Detect how npm is run and branch based on that
 switch(process.env.npm_lifecycle_event) {
-  case 'build':
-    config = merge(
-        common,
-        {
-            devtool: 'source-map',
-            output: {
-                path: PATHS.build,
-                filename: '[name].[chunkhash].js',
-                // This is used for require.ensure. The setup will work without but this is useful to set.
-                chunkFilename: '[chunkhash].js'
-            }
-        },
-        parts.clean(PATHS.build),
-        parts.setFreeVariable(
-            'process.env.NODE_ENV',
-            'production'
-        ),
-        parts.extractBundle({
-            name: 'vendor'
-        }),
-        parts.minify(),
-        parts.extractCSS(PATHS.mainscss),
-        parts.purifyCSS([PATHS.mainscss])
-    );
+    case 'build':
+        config = merge(
+            common,
+            {
+                devtool: 'source-map',
+                output: {
+                    path: PATHS.build,
+                    filename: '[name].[chunkhash].js',
+                    // This is used for require.ensure. The setup will work without but this is useful to set.
+                    chunkFilename: '[chunkhash].js'
+                }
+            },
+            parts.clean(PATHS.build),
+            parts.setFreeVariable(
+                'process.env.NODE_ENV',
+                'production'
+            ),
+            parts.extractBundle({
+                name: 'vendor'
+            }),
+            parts.minify(),
+            parts.extractCSS(PATHS.mainscss),
+            parts.purifyCSS([PATHS.mainscss])
+        );
     break;
-  default:
-    config = merge(
-        common,
-        {
-            devtool: 'eval-source-map'
-        },
-        parts.devServer({
-            // Customize host/port here if needed
-            host: process.env.HOST,
-            port: process.env.PORT
-        }),
-        parts.setupSCSS(PATHS.mainscss)
-    );
+    default:
+        config = merge(
+            common,
+            {
+                devtool: 'eval-source-map'
+            },
+            parts.devServer({
+                // Customize host/port here if needed
+                host: process.env.HOST,
+                port: process.env.PORT
+            }),
+            parts.setupSCSS(PATHS.mainscss)
+        );
 }
 
 module.exports = validate(config);
